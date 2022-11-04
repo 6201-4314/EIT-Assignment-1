965b50804ad0b3dc1940fb405a77fdae7519be60
Nazmul Hossain <64454419+6201-4314@users.noreply.github.com>
2022-11-04 22:45:10 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+//Task related to Array:
+
+
+
+
+
+//Task related to Object
+
+let persons = {
+    firstName: "Nazmul",
+    lastName: "Hossain",
+    age: 22,
+    favColor: "Black"
+};
+console.log(Object.entries(persons));        //  entries() method
+//output
+// [
+//   [ 'firstName', 'Nazmul' ],
+//   [ 'lastName', 'Hossain' ],
+//   [ 'age', 22 ],
+//   [ 'favColor', 'Black' ]
+// ]
+
+
+console.log(Object.hasOwn(persons, "age")); //true       //hasOwn()method
+console.log(Object.hasOwn(persons, "name")); //false     //hasOwn()method
+
+console.log(Object.keys(persons));        //krys() method
+
+console.log(Object.values(persons));      //values() method
+
+
+let info = {...persons };        //spread operator
+console.log(info);
+//output
+// {
+//   firstName: 'Nazmul',
+//   lastName: 'Hossain',
+//   age: 22,
+//   favColor: 'Black'
+// }
+
+
+
+
+
+//Task related to Loop
+
 // for loop
 for(let i = 0; i<10; i++){
    console.log(i)
