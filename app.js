7560ef0fcb93e322c471eacbc2af878499b1d160
Nazmul Hossain <64454419+6201-4314@users.noreply.github.com>
2022-11-04 22:21:39 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// for loop
 for(let i = 0; i<10; i++){
    console.log(i)
+}
+//for-of loop
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+for (let fruit of fruits) {
+    console.log("My Favourite Fruit : " + fruit);
+}
+
+//for-in loop
+let persons = {
+    firstName: "Nazmul",
+    lastName: "Hossain",
+    age: 22,
+    favColor: "Black"
+};
+for (let id in persons) {
+    console.log( id + " : " + persons[id]);
 }
