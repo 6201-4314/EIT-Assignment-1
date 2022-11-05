c030d40b61a8c5414ad16a1e60067888364845b7
Nazmul Hossain <64454419+nazmul-rdo@users.noreply.github.com>
2022-11-05 11:17:08 +0600

    Update map.js

--- map.js
+++ map.js
@@ -8,3 +8,15 @@ let b = a.map((num) => {
 for (let nums of b) {
     console.log(nums);
 }
+
+
+// adelt age 
+let a = [10, 23, 22, 1, 3, 4, 32]
+let b = a.map((num, i) => {
+    if (num >= 18) {
+        console.log(a[i] + " adelt age")
+    } else {
+        console.log(a[i] + " not adelt age");
+    }
+
+})
